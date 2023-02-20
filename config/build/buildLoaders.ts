434b3d98d6fa91e351832e4e9751b34a8e55697b
maria-knst <konstantinovamaria25@gmail.com>
2023-02-20 17:15:15 +0300

    Add babel, add some components

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,40 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions) : webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        }
+                    ],
+                ],
+            }
+        }
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
     const cssLoader =  {
             test: /\.s[ac]ss$/i,
             use: [
@@ -32,6 +66,9 @@ export function buildLoaders({isDev}: BuildOptions) : webpack.RuleSetRule[] {
         };
 
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React, {Suspense} from 'react';
+import './styles/index.scss';
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+
+
+const App = () => {
+    // @ts-ignore
+    const {theme} = useTheme();
+
+    return (
+        <div className={classNames('app',{},[theme])}>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className='content-page'>
+                    <Sidebar/>
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
