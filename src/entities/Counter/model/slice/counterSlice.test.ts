453a3a1487126fd6680818f1a188c4fc1ac48509
maria-knst <konstantinovamaria25@gmail.com>
2023-04-20 18:58:13 +0300

    Add redux store, firts entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,22 @@
+import { CounterSchema } from '../types/counterSchema';
+import { counterReducer, counterActions } from './counterSlice';
+
+describe('counterSlice.test', () => {
+    test('increment', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(counterReducer(state, counterActions.increment)).toEqual({ value: 11 });
+    });
+
+    test('decrement', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(counterReducer(state, counterActions.decrement)).toEqual({ value: 9 });
+    });
+
+    test('with empty state', () => {
+        expect(counterReducer(undefined, counterActions.decrement)).toEqual({ value: -1 });
+    });
+});
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
+import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
         <div>
-            <BugButton />
             {t('main')}
+            <Counter />
         </div>
     );
 };
